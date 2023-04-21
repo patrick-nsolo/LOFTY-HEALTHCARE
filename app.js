89609e78f430f909e742a89689ca773ae494a65f
Patrick Nsolo <64790572+patrick-nsolo@users.noreply.github.com>
2023-04-22 00:00:01 +0100

    update js
    update App.js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 const navMenu = document.querySelector(".dropdown-menu");
+const OverLay = document.querySelector(".overlay");
+const Burger = document.querySelector(".burger");
+
+Burger.addEventListener("click", () =>{
+    navMenu.classList.add("dropdown-open");
+    navMenu.classList.add("overlay-open");
+})
