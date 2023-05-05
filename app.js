2dfd280d17c951805edb82fda09e0baeb4ffeacd
Patrick Nsolo <64790572+patrick-nsolo@users.noreply.github.com>
2023-05-05 14:00:33 +0100

    update JS
    update CSS

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-const dropDownMenu = document.querySelector(".dropdown-menu");
-const overLay = document.querySelector(".overlay");
-const navBtn = document.querySelector(".btn");
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".menu");
 
-navBtn.addEventListener("click", () =>{
-    dropDownMenu.classList.add("dropdown-open");
-    overLayer.classList.add("overlay-open");
-});
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n =>
+    n.addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+    }))
