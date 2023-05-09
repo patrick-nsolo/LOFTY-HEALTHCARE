11eb5d68b0c70b4d4b54e4b7c626b157fd65b01a
Patrick Nsolo <64790572+patrick-nsolo@users.noreply.github.com>
2023-05-09 13:46:10 +0100

    update webpage JS
    update webpage JS

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".menu");
+const btn = document.querySelector(".btn");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
+    btn.classList.toggle("active");
 })
 
 document.querySelectorAll(".nav-link").forEach(n =>
     n.addEventListener("click", () => {
         hamburger.classList.remove("active");
         navMenu.classList.remove("active");
+        btn.classList.toggle("active");
     }))
 
 
+
+    
 var button = document.querySelector('.button');
 var join = document.querySelector('.hero-btn');
 var register = document.querySelector('.reg-btn');
