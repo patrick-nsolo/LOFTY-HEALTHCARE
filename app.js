34c4e62fff7598ddfdb0a1957024a697006148eb
Patrick Nsolo <64790572+patrick-nsolo@users.noreply.github.com>
2023-05-23 12:04:20 +0100

    update App.js navigation settings
    working fine

---
+++ app.js
@@ -0,0 +1,22 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".menu");
+const btn = document.querySelector(".btn");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+    btn.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n =>
+n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+    btn.classList.toggle("active");
+}))
+
+
+
+
+
+
