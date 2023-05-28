c105f31a3e9648de8e23ed074c5d377f87011684
Patrick Nsolo <64790572+patrick-nsolo@users.noreply.github.com>
2023-05-28 18:34:38 +0100

    update CSS
    update CSS

--- app.js
+++ app.js
@@ -28,13 +28,29 @@ var swiper = new Swiper(".mySwiper",{
     pagination:{
         el: ".swiper-pagination",
         clickable: true,
+        type: "fraction",
+    },
+    autoplay:{
+        delay:3000,
+        disableOnInteraction:false,
     },
     navigation:{
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
+    effect: "creative", 
+    creativeEffect:{
+        prev:{
+            shadow:true,
+            translate:[0,0-400],
+        },
+        next:{
+            translate:["100",0,0],
+
+        },
+    }
 });
-setInterval(changeImage, 3000);
+
 //AUTOMATIC SLIDER SCRIPT END
 
 
