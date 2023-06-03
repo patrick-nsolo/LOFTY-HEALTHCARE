1783687fb6aba66b526d639ab453312b4e2e1276
Patrick Nsolo <64790572+patrick-nsolo@users.noreply.github.com>
2023-06-03 14:08:12 +0100

    Database creation and connection

---
+++ registration-submit.php
@@ -0,0 +1,10 @@
+<?php 
+//establish DB connection
+$servername = "localhost";
+$username = "root";
+$password = "1234";
+$dbname = "registrationDB";
+
+$conn = new mysqli($servername,$username,$password,$dbname);
+
+?>
