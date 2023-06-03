c7f3476b95762d903c24d367b03543646d1826f9
Patrick Nsolo <64790572+patrick-nsolo@users.noreply.github.com>
2023-06-03 19:40:28 +0100

    update form

--- registration-submit.php
+++ registration-submit.php
@@ -18,6 +18,7 @@ $password = $_POST['password'];
 $confirmPassword = $_POST['confirm-password'];
 $firstName = $_POST['first-name'];
 $surname = $_POST['surname'];
+$gender = $_POST['gender'];
 $email = $_POST['email'];
 $countryCode = $_POST['country-code'];
 $phoneNumber = $_POST['phone-number'];
@@ -25,8 +26,8 @@ $address = $_POST['address'];
 $profession = $_POST['profession'];
 
 //Insert formdata into DB
-$sql = "INSERT INTO registrations (userName, password, confirmPassword, firstName, surname, email, countryCode, phoneNumber, address, profession )
-        VALUES ($userName, $password, $confirmPassword, $firstName, $surname, $email, $countryCode, $phoneNumber, $address, $profession )";
+$sql = "INSERT INTO registrations (userName, password, confirmPassword, firstName, surname, gender, email, countryCode, phoneNumber, address, profession )
+        VALUES ($userName, $password, $confirmPassword, $firstName, $surname, $gender, $email, $countryCode, $phoneNumber, $address, $profession )";
 
 if ($conn->query(sql) === true){
     //registration successful
