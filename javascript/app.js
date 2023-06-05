0f8d5a7da53f212e4c98cd4dca484b1fc1638315
Patrick Nsolo <64790572+patrick-nsolo@users.noreply.github.com>
2023-06-05 01:28:46 +0100

    update form database connect

--- javascript/app.js
+++ javascript/app.js
@@ -68,91 +68,10 @@ function togglePasswordVisibility(){
         passwordInput.type = "password";
     }
 }  
-//FORM 
+//FORM ENTRY POINT SCRIPT
 const express = require('express');
-const mysql = require('mysql2');
-const bodyParser = require('body-parser'); 
-const multer = require('multer');
-const path = require('path');
-
-//create mysql DB connection
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Ed/12c/2625',
-    database: 'registrants',
-    authPlugins: {}
-});
-//connect to DB
-db.connect((err) => {
-    if(err){
-        throw err;
-    }
-    console.log('Connected to the database');
-});
-//create Express app
-const app = express();
-//set up middleware
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json());
-//set up multer for file uploads
-const storage = multer.diskStorage({
-    destination: './uploads',
-    filename: (req, file, cb) => {
-        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-    },
-});
-const upload = multer({storage});
-//Serve static files from the 'public' directory
-app.use(express.static('public'));
-
-//Define routes
-
-//Handle form submission
-app.post('/registration-submit', upload.fields([{name: 'cv', maxCount: 1 }, { name: 'picture', maxCount: 1 }]), (req,res) =>{
-    const{
-        user_name,
-        password,
-        confirm_password,
-        first_name,
-        surname,
-        gender,
-        email,
-        country_code,
-        phone_number,
-        address,
-        profession,
-    } = req.body
-
-    //Perform form validation
-    if (!user_name || !password || !confirm_password || !first_name || !surname || !gender || !email || !country_code || !phone_number || !address || !profession){
-        return res.status(400).json({error: 'Please fill in all fields'});
-    }
-    if (password !== confirm_password){
-        return res.status(400).json({error: 'Passwords do not match'});
-    }
-    //save form data to DB
-    const query = 'INSERT INTO users (user_name, password, first_name, surname, gender, email, country_code, phone_number, address, profession, cv_path, picture_path) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
-    const cvPath = req.files.cv ? req.files.cv[0].path : '';
-    const picturePath = req.files.pictures ? req.files.picture[0].path : '';
-    
-    db.query(query, [user_name, password, first_name, surname, gender, email, country_code, phone_number, address, profession, cvPath, picturePath], (err, result) => {
-        if (err){
-            return res.status(500).json({error: 'Failed to register user'});    
-        }
-        //registeration successful
-        res.status(200).json({message: 'User registered successfully'});
-
-    });
-});
-//start server
-app.listen(3000,() => {
-    console.log('server started on port 3000');
-});
-
-
-
-
-
-
-
+const bodyParser = require('body-parser');
+const routes = require('../javascript/route');
+app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(''))
